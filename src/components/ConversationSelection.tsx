16b3d18e27828d0a2afd479bd05ed8d20daa277b
Eternityyyyyt <stoneziheng@126.com>
2024-05-07 08:35:41 +0800

    Feat:display conversation latest message time with date(day&month) if not today(#17)

--- src/components/ConversationSelection.tsx
+++ src/components/ConversationSelection.tsx
@@ -27,16 +27,35 @@ const ConversationSelection: React.FC<ConversationSelectionProps> = ({
       return str;
     }
   function formattime(timestamp:number) {
+    const now = new Date();
     const seconds = Math.floor(timestamp);
     // 格式化时间戳为易读的时间格式
     if(timestamp == 0){return ''}
-
-    const formattedTime = new Date(seconds * 1000).toLocaleTimeString('zh-CN', {
+    const date = new Date(seconds*1000);
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const yesterday = new Date(today);
+    yesterday.setDate(today.getDate() - 1);
+    const dayBeforeYesterday = new Date(today);
+    dayBeforeYesterday.setDate(today.getDate() - 2);
+    
+    if (date.toDateString() === today.toDateString()) {
+      // 如果是今天，显示时间
+      const formattedTime = new Date(seconds * 1000).toLocaleTimeString('zh-CN', {
       hour: '2-digit',
       minute: '2-digit',
     });
     return `(${formattedTime})`
+    } else if (date.toDateString() === yesterday.toDateString()) {
+      return "昨天";
+    } else if (date.toDateString() === dayBeforeYesterday.toDateString()) {
+      return "前天";
+    } else {
+      // 否则显示具体日期（只需要月份和年份）
+      return `${date.getMonth() + 1}月${date.getDate()}日`;
+    }
   }
+    
+  
   const [avatars, setAvatars] = useState<Record<number, string>>({});
   
   const [latestMessages, setLatestMessages] = useState<Record<number, string>>({});
